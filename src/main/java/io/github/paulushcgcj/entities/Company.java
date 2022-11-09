7ddcbfd292eb56f6d2f02d92fb7d15246fc35b8e
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-11-09 09:29:56 -0800

    feature(l2cache): changed cache to use redisson

--- src/main/java/io/github/paulushcgcj/entities/Company.java
+++ src/main/java/io/github/paulushcgcj/entities/Company.java
@@ -5,17 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.domain.Persistable;
-
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-
-import static javax.persistence.GenerationType.IDENTITY;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -27,7 +23,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Entity
 @Table(name = "companies",schema = "company")
 @Cacheable
-@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@org.hibernate.annotations.Cache(region = "companyCache",usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Company implements Persistable<String> {
 
   @Id
